1b7ed3b50e849053e0f40c6cfe878ccb6f207be2
RAHUL A P <rahul.ap@aot-technologies.com>
2025-03-08 22:32:42 -0800

    tailwind setup

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text> app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
